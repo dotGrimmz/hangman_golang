31534bb24f73b1e0ec8915d832f488aebe926d1f
dotGrimmz <rakeemxng@gmail.com>
2022-07-05 18:31:15 -0400

    met all AC

--- main.go
+++ main.go
@@ -12,142 +12,157 @@ type Options struct {
 }
 
 type GameData struct {
-	selectedTheme, wordToGuess, displayValue, alreadyGuessed string
-	userGuess string
-	guessCount, incorrectGuessCount int 
-	incorrectGuessLimit int
+	selectedTheme, wordToGuess, displayValue, alreadyGuessed, userGuess string
+	guessCount, incorrectGuessCount                                     int
+	incorrectGuessLimit                                                 int
 }
 
 type GameHistory struct {
-	data [] GameData
+	data []GameData
 }
-
-
 
 func main() {
 
-
-
-	 gameData := GameData{}
-	 themes:= []string {"data/presidents.json" ,"data/cars.json"  ,"data/anime.json"}
-
+	gameData := GameData{}
+	themes := []string{"data/presidents.json", "data/cars.json", "data/anime.json"}
 
 	getStartInto(&gameData)
 
-
-	gameData.wordToGuess =	getWordFromTheme("cars", themes)
+	gameData.wordToGuess = getWordFromTheme("cars", themes)
 	gameData.displayValue = createDefaultDisplay(len(gameData.wordToGuess))
-
 
 	handleUserGuess(&gameData)
 
 	handleDisplay(&gameData)
 
-
-
 	for {
 		gameComplete := !strings.Contains(gameData.displayValue, "_")
 		gameOver := (gameData.incorrectGuessLimit - gameData.incorrectGuessCount) <= 0
+		userQuit := gameData.userGuess == "QUIT"
+
 		if gameComplete {
 			fmt.Printf("Good Job! \n %v\n", gameData.wordToGuess)
 			fmt.Printf("Here are your stats: %+v", gameData)
 			break
-			}
-		if gameOver	{
+		}
+		if gameOver {
 			fmt.Println("Sorry, Too many incorrect attempts")
 			break
 		}
 
+		if userQuit {
+			fmt.Print("User quit")
+			break
+		}
 		handleUserGuess(&gameData)
 		handleDisplay(&gameData)
-		}
 
 	}
-
-	func handleUserGuess (game *GameData) {
-		fmt.Println(game.displayValue)
-		fmt.Println("Guess a letter ")
-		fmt.Scan(&game.userGuess)
-
-		//if len(game.userGuess)
-
-		current := strings.ToUpper(game.userGuess)
-		alreadyUsed := strings.Contains(game.alreadyGuessed, current)
-		if alreadyUsed {
-			fmt.Printf("You have already Guessed: %v \n", game.userGuess)
-			 handleUserGuess(game)
-		}
-			game.alreadyGuessed += strings.ToUpper(game.userGuess)
-
-	}
-
-	func createDefaultDisplay (lengthOfWord int) string{
-		defaultDisplay :=""
-		for i := 0; i < lengthOfWord; i++ {
-			defaultDisplay += "_"
-		}
-		return defaultDisplay
-	}
-
-
-
-	func handleDisplay (game *GameData)  {
-		var listOfIndexes []int
-		x := strings.ToUpper(game.userGuess)
-		newGuess := []rune(x)[0]
-
-		for i, char := range game.wordToGuess {
-			if char == newGuess {
-				listOfIndexes=	append(listOfIndexes, i)
-			}
-		}
-
-		decodedDisplayVal := []rune(game.displayValue)
-		correctGuess := len(listOfIndexes) > 0
-
-		if correctGuess {
-			for i, _ := range listOfIndexes {
-				//range through the list of indexs and then assign each item and
-				// and replace it with the guessed value
-				decodedDisplayVal[listOfIndexes[i]] = newGuess
-			}
-			game.displayValue = string(decodedDisplayVal)
-			game.guessCount ++
-		}  else  {
-			game.incorrectGuessCount ++
-			fmt.Printf("Number of Incorrect guesses left: %v \n", game.incorrectGuessLimit - game.incorrectGuessCount)
-		}
 
 }
 
-	func getWordFromTheme (selectedThemeStr string, options []string ) string {
+func handleUserGuess(game *GameData) {
+	fmt.Println(game.displayValue)
+	fmt.Println("Guess a letter ")
+
+	fmt.Scan(&game.userGuess)
+
+	current := strings.ToUpper(game.userGuess)
+	game.userGuess = current
+	if game.userGuess == "QUIT" {
+		fmt.Println("Sorry to see you go!")
+		return
+	}
+
+	if len(game.userGuess) > 1 {
+		fmt.Println("Please enter only one letter")
+		handleUserGuess(game)
+	}
+
+	alreadyUsed := strings.Contains(game.alreadyGuessed, current)
+	if alreadyUsed {
+		fmt.Printf("You have already Guessed: %v \n", game.userGuess)
+		handleUserGuess(game)
+	} else {
+		game.alreadyGuessed += strings.ToUpper(game.userGuess)
+	}
+
+}
+
+func createDefaultDisplay(lengthOfWord int) string {
+	defaultDisplay := ""
+	for i := 0; i < lengthOfWord; i++ {
+		defaultDisplay += "_"
+	}
+	return defaultDisplay
+}
+
+func handleDisplay(game *GameData) {
+	var listOfIndexes []int
+	x := strings.ToUpper(game.userGuess)
+	newGuess := []rune(x)[0]
+
+	for i, char := range game.wordToGuess {
+		if char == newGuess {
+			listOfIndexes = append(listOfIndexes, i)
+		}
+	}
+
+	decodedDisplayVal := []rune(game.displayValue)
+	correctGuess := len(listOfIndexes) > 0
+
+	if correctGuess {
+		for i, _ := range listOfIndexes {
+			//range through the list of indexs and then assign each item and
+			// and replace it with the guessed value
+			decodedDisplayVal[listOfIndexes[i]] = newGuess
+		}
+		game.displayValue = string(decodedDisplayVal)
+		game.guessCount++
+	} else {
+		game.incorrectGuessCount++
+		fmt.Printf("Number of Incorrect guesses left: %v \n", game.incorrectGuessLimit-game.incorrectGuessCount)
+	}
+
+}
+
+func getWordFromTheme(selectedThemeStr string, options []string) string {
 	selectedWord := ""
 	for _, val := range options {
 		if strings.Contains(val, selectedThemeStr) {
-		words, _ :=	ParseFromJson(val)
-		selectedWord = strings.ToUpper(words.Data[0])
-			}
+			words, _ := ParseFromJson(val)
+			selectedWord = strings.ToUpper(words.Data[0])
 		}
-		return selectedWord
+	}
+	return selectedWord
 }
 
-	func getStartInto (game *GameData)    {
-		difficultyLevels := map[string] int {"EASY" : 10, "MEDIUM": 6, "HARD": 4}
-		fmt.Println("Welcome to Hangman! \nChoose your difficulty level: \n ")
+func getStartInto(game *GameData) {
+	difficultyLevels := map[string]int{}
+	fmt.Println("Welcome to Hangman! \nChoose your difficulty level: \n ")
 
-		for k, val := range difficultyLevels {
+	file, err := ioutil.ReadFile("data/difficultyLevels.json")
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(file, &difficultyLevels)
+	if err != nil {
+		panic(err)
+	}
+
+	for k, val := range difficultyLevels {
 		fmt.Printf("Enter %v for %v Attempts \n", k, val)
 	}
 	var difficultyChoice string
 	fmt.Scan(&difficultyChoice)
-		game.incorrectGuessLimit = difficultyLevels[difficultyChoice]
-
+	game.incorrectGuessLimit = difficultyLevels[difficultyChoice]
 
 	fmt.Printf("\n \nChoose your theme: \n Cars \n Presidents \n Anime\n")
 	fmt.Scan(&game.selectedTheme)
 }
 
-	func ParseFromJson (fileName string) (Options, error) {
+func ParseFromJson(fileName string) (Options, error) {
 	file, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
@@ -158,12 +173,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	return data, err
-	
+
 }
 
-	func ParseToJson(data Options, fileName string) bool {
+func ParseToJson(data Options, fileName string) bool {
 	jsonFile, err := json.MarshalIndent(data, "", " ")
 	if err != nil {
 		panic(err)
